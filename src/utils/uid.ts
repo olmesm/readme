eb365f8791d92850c2065cdc14228115a2e13b83
Oliver Smit <olmesm@gmail.com>
2022-08-09 17:30:06 +0100

    better uid

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -1,1 +1,5 @@
-export const uid = () => Date.now().toString();
+export const uid = () => {
+  //   from https://levelup.gitconnected.com/generate-unique-id-in-the-browser-without-a-library-50618cdc3cb1
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+  return uint32.toString(16);
+};
