c4d16030f81eca3c08e389a9e0cb396668939b9b
Oliver Smit <olmesm@gmail.com>
2022-08-25 10:54:10 +0100

    added readme

--- scripts/create-bootstrap-custom-template-site.sh
+++ scripts/create-bootstrap-custom-template-site.sh
@@ -6,8 +6,10 @@ ARCHIVE_FILE="dist/custom-templates.zip"
 echo ">> Making tmp directory"
 mkdir -p $DIRECTORY_TMP
 
-echo ">> Copying static site"
+echo ">> Copying static site files"
 cp custom-templates/* $DIRECTORY_TMP
+cp LICENSE $DIRECTORY_TMP
+cp public/favicon.ico $DIRECTORY_TMP
 
 echo ">> Copying templates"
 cp -r public/templates $DIRECTORY_TMP
